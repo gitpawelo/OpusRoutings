67bdfac3d75c949cf96eca69bf2bc9f8cc10a44b
pawelpawel8787 <gitpawelo@wp.pl>
2019-04-28 22:09:46 +0200

    first working test version

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,14 @@
 package sample;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+
+import java.io.*;
 
 public class Controller {
 
@@ -14,12 +19,69 @@ public class Controller {
     private Button createJsonBtn;
 
     @FXML
-    private TextField TpaTextField;
+    private TextField tpaTextField;
 
     @FXML
-    private TextField JsonTextField;
+    private TextField jsonTextField;
 
     @FXML
-    private TextField OCcustIdListView;
+    private ListView ocCustIdListView;
+
+    public void buttonBrowse(ActionEvent event) throws Exception {
+
+        FileChooser fc = new FileChooser();
+        File selectedFile = new File(fc.showOpenDialog(null).toString());
+
+        String path = String.valueOf(selectedFile);
+        if (selectedFile != null){
+ //           ocCustIdListView.getItems().add(selectedFile.getAbsoluteFile());
+            BufferedReader reader = null;
+            BufferedWriter writer = null;
+
+            String pathOut = "C:\\Users\\Pawel\\Desktop\\output.txt";
+
+            System.out.println("ściezka: " + path);
+//            path.replace('', 'D');
+            path = path.replace("\\", "\\\\");
+            System.out.println("ściezka: " + path);
+
+            try {
+                reader = new BufferedReader(new FileReader(path));
+                writer = new BufferedWriter(new FileWriter(pathOut));
+
+                String line1;
+
+                while ((line1 = reader.readLine()) != null) {
+                    writer.write(line1
+                            .replace("WItaj", "Hello")
+
+                    );
+
+                    writer.append("\n");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    reader.close();
+                }
+                if (writer != null) {
+                    writer.close();
+                }
+            }
+        }else
+            System.out.println("file is not valid");
+
+    }
+
+    public void ButtonCreateJson(ActionEvent event) throws Exception{
+
+
+
+
+    }
+
+
 
 }
